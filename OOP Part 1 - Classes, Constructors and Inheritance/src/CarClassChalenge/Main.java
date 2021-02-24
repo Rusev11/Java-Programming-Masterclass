e73e42770571f2ab3165d640a6c509cd34c97757
Ivaylo Rusev <ivaylo.rusev@outlook.com>
2021-02-24 15:21:40 +0200

    Vehicle Challenge completed

---
+++ OOP Part 1 - Classes, Constructors and Inheritance/src/CarClassChalenge/Main.java
@@ -0,0 +1,10 @@
+package CarClassChalenge;
+
+public class Main {
+    public static void main(String[] args) {
+        Outlander outlander = new Outlander(5);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+    }
+}
