691d75188e60ae22f084a7fd61b8b28075bb9981
Ivaylo Rusev <ivaylo.rusev@outlook.com>
2021-03-08 23:31:12 +0200

    Some additional code added to Team Challenge

--- Java Generics/src/Teams/Main.java
+++ Java Generics/src/Teams/Main.java
@@ -2,39 +2,58 @@ package Teams;
 
 public class Main {
     public static void main(String[] args) {
-        FootballPlayer beckham = new FootballPlayer("Beckham");
-        BaseballPlayer pat = new BaseballPlayer("Pat");
-        BasketballPlayer joe = new BasketballPlayer("Joe");
+//        FootballPlayer beckham = new FootballPlayer("Beckham");
+//        BaseballPlayer pat = new BaseballPlayer("Pat");
+//        BasketballPlayer joe = new BasketballPlayer("Joe");
+//
+//        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
+//        adelaideCrows.addPlayer(beckham);
+//
+//        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+//        baseballTeam.addPlayer(pat);
+//
+//        Team<BasketballPlayer> basketballTeam = new Team<>("Miami Heats");
+//        basketballTeam.addPlayer(joe);
+//
+//        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+//        FootballPlayer bank = new FootballPlayer("Gordon");
+//
+//        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+//        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+//
+//        hawthorn.matchResult(fremantle, 1, 0);
+//        hawthorn.matchResult(adelaideCrows, 3, 8);
+//
+//        adelaideCrows.matchResult(fremantle, 2, 1);
+//
+//        System.out.println("Rankings");
+//        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+//        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+//        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+//        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+//
+//        System.out.println(adelaideCrows.compareTo(melbourne));
+//        System.out.println(adelaideCrows.compareTo(hawthorn));
+//        System.out.println(hawthorn.compareTo(adelaideCrows));
+//        System.out.println(melbourne.compareTo(fremantle));
+
+        League<Team<FootballPlayer>> footballLeague = new League<>("AFL");
 
         Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
-        adelaideCrows.addPlayer(beckham);
-
-        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
-        baseballTeam.addPlayer(pat);
-
-        Team<BasketballPlayer> basketballTeam = new Team<>("Miami Heats");
-        basketballTeam.addPlayer(joe);
-
         Team<FootballPlayer> melbourne = new Team<>("Melbourne");
-        FootballPlayer bank = new FootballPlayer("Gordon");
-
         Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
         Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+        Team<BasketballPlayer> basketballTeam = new Team<>("Miami Heats");
+
+        footballLeague.add(adelaideCrows);
+        footballLeague.add(melbourne);
+        footballLeague.add(hawthorn);
+        footballLeague.add(fremantle);
 
         hawthorn.matchResult(fremantle, 1, 0);
         hawthorn.matchResult(adelaideCrows, 3, 8);
-
         adelaideCrows.matchResult(fremantle, 2, 1);
 
-        System.out.println("Rankings");
-        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
-        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
-        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
-        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
-
-        System.out.println(adelaideCrows.compareTo(melbourne));
-        System.out.println(adelaideCrows.compareTo(hawthorn));
-        System.out.println(hawthorn.compareTo(adelaideCrows));
-        System.out.println(melbourne.compareTo(fremantle));
+        footballLeague.showLeagueTable();
     }
 }
