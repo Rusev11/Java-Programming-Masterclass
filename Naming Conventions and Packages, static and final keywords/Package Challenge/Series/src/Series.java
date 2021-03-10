a0112688693cc7585062692f83db302abdfc4a42
Ivaylo Rusev <ivaylo.rusev@outlook.com>
2021-03-10 16:52:46 +0200

    Package Challenge Completed

---
+++ Naming Conventions and Packages, static and final keywords/Package Challenge/Series/src/Series.java
@@ -0,0 +1,25 @@
+public class Series {
+    public static int nSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int factorial(int n) {
+        int product = 1;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
