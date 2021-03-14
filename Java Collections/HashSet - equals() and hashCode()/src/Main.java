2f7a9c0f82954ac768fee6783a0f59b992354370
Rusev11 <ivaylo.rusev@outlook.com>
2021-03-14 17:01:20 +0200

    equials() and hashCode() example implemented

--- Java Collections/HashSet - equals() and hashCode()/src/Main.java
+++ Java Collections/HashSet - equals() and hashCode()/src/Main.java
@@ -4,7 +4,6 @@ import java.util.Map;
 import java.util.Set;
 
 public class Main {
-
     private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
     private static Set<HeavenlyBody> planets = new HashSet<>();
 
@@ -74,25 +73,37 @@ public class Main {
         planets.add(temp);
 
         System.out.println("Planets");
-        for (HeavenlyBody planet : planets) {
+        for(HeavenlyBody planet : planets) {
             System.out.println("\t" + planet.getName());
         }
 
         HeavenlyBody body = solarSystem.get("Mars");
         System.out.println("Moons of " + body.getName());
-        for (HeavenlyBody jupiterMoon : body.getSatellites()) {
+        for(HeavenlyBody jupiterMoon: body.getSatellites()) {
             System.out.println("\t" + jupiterMoon.getName());
         }
 
         Set<HeavenlyBody> moons = new HashSet<>();
-        for (HeavenlyBody planet : planets) {
+        for(HeavenlyBody planet : planets) {
             moons.addAll(planet.getSatellites());
         }
 
         System.out.println("All Moons");
-        for (HeavenlyBody moon : moons) {
+        for(HeavenlyBody moon : moons) {
             System.out.println("\t" + moon.getName());
         }
+
+        HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
+        planets.add(pluto);
+
+        for(HeavenlyBody planet : planets) {
+            System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
+        }
+
+        Object o = new Object();
+        o.equals(o);
+        "pluto".equals("");
+
 
 
     }
