d932251729e7441f26641cfaa86a58b4522dc7b6
Rusev11 <ivaylo.rusev@outlook.com>
2021-03-18 21:22:05 +0200

    DB Test Project

---
+++ Databases/Creating Databases With JDBC in Java/src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:DB\\testjava.db");
+        } catch (SQLException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+   
+    }
+}
