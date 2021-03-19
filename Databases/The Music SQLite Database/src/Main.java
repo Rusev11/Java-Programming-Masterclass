9a59cfddcab0d7b0e0bbcf16d5a1ecd1111a0d7e
Rusev11 <ivaylo.rusev@outlook.com>
2021-03-19 13:07:53 +0200

    model package created. Datasource Class created

--- Databases/The Music SQLite Database/src/Main.java
+++ Databases/The Music SQLite Database/src/Main.java
@@ -1,5 +1,14 @@
+import model.Datasource;
+
 public class Main {
     public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()){
+            System.out.println("Cannot open datasource");
+            return;
+        }
 
+
+        datasource.close();
     }
 }
