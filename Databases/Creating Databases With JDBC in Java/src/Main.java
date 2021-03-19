04a8f040d2aad95bbe3f8628849e7347289c1e4c
Rusev11 <ivaylo.rusev@outlook.com>
2021-03-19 11:49:09 +0200

    SQLite DB Insert, Update, Delete, .executeQuery() and Constants example completed

--- Databases/Creating Databases With JDBC in Java/src/Main.java
+++ Databases/Creating Databases With JDBC in Java/src/Main.java
@@ -1,15 +1,22 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:DB\\testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS contacts" +
+                                  "(name TEXT, phone INTEGER, email TEXT)");
+
+            statement.close();
+            conn.close();
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }
-   
+
     }
 }
