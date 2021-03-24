b966a16bd1eabf4b906fe74074b074465bd32a8a
Rusev11 <ivaylo.rusev@outlook.com>
2021-03-24 14:14:54 +0200

    Interupt and Join Implemented

--- Concurrency in Java/Threads/src/Main.java
+++ Concurrency in Java/Threads/src/Main.java
@@ -1,9 +1,10 @@
+
 public class Main {
     public static void main(String[] args) {
-
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread.");
-        Thread anotherThread = new AnotherThread();
-        anotherThread.setName("==Another Thread==");
+
+        final Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
         new Thread() {
@@ -12,11 +13,21 @@ public class Main {
             }
         }.start();
 
+        Thread myRunnableThread = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ThreadColor.ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ThreadColor.ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
+            }
+        });
 
-        Thread myRunnableThread = new Thread(new MyRunnable());
         myRunnableThread.start();
+
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread.");
     }
-
-
 }
